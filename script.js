10fa3b309d54eeccfd3d2f2b308851623fbdc172
Aleksey Korovin <tayrenov@gmail.com>
2024-03-20 15:24:34 +0300

    sort todos

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const todoList = document.getElementById('todo_list');
 var data = [];
 const limitToView = 5;
 var leftTodo = 0;
+var sortType='all'
+var sortData =[]
 /***** Helpers *****/
 
 function removeTodo() {
@@ -55,15 +57,51 @@ function validateInput(input) {
         return true
     }
 }
+
+/***** SORT *****/
+
+function sortTodos() {
+    console.log(this.dataset.sort)
+    sortType = this.dataset.sort;
+    printTodosFooter()
+
+    switch(this.dataset.sort) {
+        case 'all': {
+            sortData = data;
+            createTodoList(sortData)
+        }break;
+        case 'active':{
+            sortData = []
+            data.forEach((arr) => {
+                if (arr.completed == false) {
+                    sortData.push(arr)
+                }
+            })
+            console.log(sortData)
+            createTodoList(sortData)
+        }break;
+        case 'complited':{
+            sortData = []
+            data.forEach((arr) => {
+                if (arr.completed == true) {
+                    sortData.push(arr)
+                }
+            })
+            console.log(sortData)
+            createTodoList(sortData)
+        }break;
+    }
+}
 /***** DOM events *****/
 
-function createTodoList() {
+function createTodoList(data) {
     console.log(data)
-
+    todoList.innerHTML=''
     for (var i = 0; i<data.length; i++) {
         createTodoItem(data[i]);
     }
 
+    printTodosFooter()
 }
 
 function createTodoItem(data) {
@@ -87,8 +125,90 @@ function createTodoItem(data) {
     todoList.prepend(div)
     div.prepend(input)
     div.append(closeIcon)
+
+
 }
 
+function printTodosFooter() {
+    var footer = document.querySelector('.todo-items_footer');
+    footer.innerHTML='';
+
+    var leftItems = document.createElement('div');
+    leftItems.className = '_left-items';
+    leftItems.innerHTML = `<span> <span> items left`
+    console.log('leftItems')
+
+    footer.prepend(leftItems)
+
+    var sortItems = document.createElement('div');
+    sortItems.classList='_sort-items';
+
+        var allSort = document.createElement('span');
+        allSort.dataset.sort='all'
+        allSort.innerHTML='<span>All</span>'
+
+        var activeSort = document.createElement('span');
+        activeSort.dataset.sort='active'
+        activeSort.innerHTML='<span>Active</span>'
+
+        var complitedSort = document.createElement('span');
+        complitedSort.dataset.sort='complited'
+        complitedSort.innerHTML='<span>Complited</span>'
+
+
+
+        sortItems.prepend(complitedSort)
+        sortItems.prepend(activeSort)
+        sortItems.prepend(allSort)
+
+    footer.append(sortItems)
+
+    switch(sortType) {
+        case 'all': {
+            allSort.classList.add('active');
+            allSort.removeEventListener('click',sortTodos)
+
+            activeSort.classList.remove('active')
+            activeSort.addEventListener('click', sortTodos)
+
+            complitedSort.classList.remove('active')
+            complitedSort.addEventListener('click', sortTodos)
+
+            console.log('swithCase All')
+        } break;
+        case 'active': {
+            activeSort.classList.add('active');
+            activeSort.removeEventListener('click', sortTodos)
+
+            allSort.classList.remove('active')
+            allSort.addEventListener('click', sortTodos)
+
+            complitedSort.classList.remove('active')
+            complitedSort.addEventListener('click', sortTodos)
+
+            console.log('swithCase active')
+        } break;
+        case 'complited': {
+            complitedSort.classList.add('active');
+            complitedSort.removeEventListener('click', sortTodos)
+
+            allSort.classList.remove('active')
+            allSort.addEventListener('click', sortTodos)
+
+            activeSort.classList.remove('active')
+            activeSort.addEventListener('click', sortTodos)
+
+            console.log('swithCase complited')
+        } break;
+    }
+
+
+    var clearItems = document.createElement('div');
+    clearItems.className = '_clear-todos';
+    clearItems.innerHTML = 'Clear Completed';
+    clearItems.setAttribute('onclick', 'testFunc()');
+    footer.append(clearItems)
+}
 
 /***** Events *****/
 document.addEventListener('DOMContentLoaded', initApp);
@@ -106,7 +226,7 @@ document.addEventListener('DOMContentLoaded', ()=> {
 function initApp() {
     Promise.all([getAllTodos()])
         .then (values => {[data] = values;
-            createTodoList()
+            createTodoList(data)
         })
 }
 
